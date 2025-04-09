34b7f57f5ade3993ecfd2d87ae1638b482dbeaee
Enzo Luigi Schork <enzoluigischork@gmail.com>
2025-04-09 08:29:06 -0300

    Initial Commit

---
+++ FaceMatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceMatch
+//
+//  Created by Enzo Luigi Schork on 09/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceMatch/FaceMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceMatchApp.swift
+//  FaceMatch
+//
+//  Created by Enzo Luigi Schork on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FaceMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
